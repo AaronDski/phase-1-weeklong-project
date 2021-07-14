233ce1844ce6007be8b210b9b9bea4ef92972a42
Aaron Dereszynski <aarondski123@gmail.com>
2021-07-14 12:23:22 -0600

    got event lisnters

--- index.js
+++ index.js
@@ -14,25 +14,25 @@ fetch(baseUrl)
 .then(res => res.json())
 .then (data => jokeData(data))
 }
-
-
 function jokeData(startData){
     let joke = document.querySelector(".fact-text")
     joke.textContent = startData.value
     // console.log(joke)
-
-}
-
-function subBtnClick(){
+}function subBtnClick(){
     document.querySelector('#fact-form').addEventListener('submit', (e) => {
          e.preventDefault()
         let li = document.createElement('li')
         li.textContent =e.target.subject.value
-        document.querySelector('.chuck-facts').append(li)
+        let btn = document.createElement('button')
+        btn.innerText = "Remove"
+        btn.addEventListener('click', (e) => {
+            e.preventDefault()
+            li.remove()
+            btn.remove()
+        })
+        document.querySelector('.chuck-facts').append(li,btn)
         e.target.subject.value = ""
-
-         console.log(li)
-            
+        //  console.log(li)
     })
 } 
 subBtnClick();
@@ -52,7 +52,14 @@ function addToFavs (){
         let fact = document.querySelector('.fact-text').textContent
         let li = document.createElement('li')
         li.textContent = fact
-        document.querySelector('.chuck-facts').append(li)
+        let btn = document.createElement('button')
+        btn.textContent = "Remove"
+        btn.addEventListener('click', (e) => {
+            e.preventDefault()
+            li.remove()
+            btn.remove()
+        })
+        document.querySelector('.chuck-facts').append(li,btn)
         // console.log(li)
     })
 }
